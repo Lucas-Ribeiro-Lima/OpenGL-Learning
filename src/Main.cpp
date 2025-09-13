78b6b0bbe130df4cb3ef027ca9ce81f9b8e074be
lucas.lima <lucasribeirolima974@gmail.com>
2025-09-13 15:29:33 -0300

    Feat: Update remove star class

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,9 +4,8 @@
 #include "Corp.h"
 #include "ResourceManager.h"
 #include "Config.h"
-#include <vector>
 
-#include "Star.h"
+#include <vector>
 
 struct CorpData {
     MaterialData material;
@@ -17,17 +16,23 @@ struct CorpData {
 };
 
 std::vector<CorpData> planets{
-    {MaterialData{ASSETS_DIR "mercury.jpg"}, glm::vec3(-30.0f, 0.0f, -30.0f), 2.0f, 5.0f, 0.8f}, // Mercury
-    {MaterialData{ASSETS_DIR "venus.jpg"}, glm::vec3(45.0f, 0.0f, 45.0f), 3.0f, 3.0f, 0.5f}, // Venus
+    {{ASSETS_DIR "mercury.jpg"}, {-30.0f, 0.0f, -30.0f}, 2.0f, 5.0f, 0.8f}, // Mercury
+    {{ASSETS_DIR "venus.jpg"}, {45.0f, 0.0f, 45.0f}, 3.0f, 3.0f, 0.5f}, // Venus
     {
-        MaterialData{ASSETS_DIR "earth.jpg", ASSETS_DIR "earth_specular_map.tif", ASSETS_DIR "earth_nightmap.jpg"},
-        glm::vec3(-60.0f, 0.0f, -60.0f), 3.3f, 8.0f, 0.2f
+        {ASSETS_DIR "earth.jpg", ASSETS_DIR "earth_specular_map.tif", ASSETS_DIR "earth_nightmap.jpg"},
+        {-60.0f, 0.0f, -60.0f}, 3.3f, 8.0f, 0.2f
     }, // Earth
-    {MaterialData{ASSETS_DIR "mars.jpg"}, glm::vec3(75.0f, 0.0f, 75.0f), 2.5f, 7.0f, 0.1f}, // Mars
-    {MaterialData{ASSETS_DIR "jupiter.jpg"}, glm::vec3(-110.0f, 0.0f, -110.0f), 12.0f, 12.0f, 0.3f}, // Jupiter
-    {MaterialData{ASSETS_DIR "saturn.jpg"}, glm::vec3(150.0f, 0.0f, 150.0f), 10.0f, 10.0f, 0.4f}, // Saturn
-    {MaterialData{ASSETS_DIR "uranus.jpg"}, glm::vec3(-190.0f, 0.0f, -190.0f), 4.5f, 6.0f, 0.8f}, // Uranus
-    {MaterialData{ASSETS_DIR "neptune.jpg"}, glm::vec3(230.0f, 0.0f, 230.0f), 4.0f, 5.0f, 0.1f}, // Neptune
+    {{ASSETS_DIR "mars.jpg"}, {75.0f, 0.0f, 75.0f}, 2.5f, 7.0f, 0.1f}, // Mars
+    {{ASSETS_DIR "jupiter.jpg"}, {-110.0f, 0.0f, -110.0f}, 12.0f, 12.0f, 0.3f}, // Jupiter
+    {{ASSETS_DIR "saturn.jpg"}, {150.0f, 0.0f, 150.0f}, 10.0f, 10.0f, 0.4f}, // Saturn
+    {{ASSETS_DIR "uranus.jpg"}, {-190.0f, 0.0f, -190.0f}, 4.5f, 6.0f, 0.8f}, // Uranus
+    {{ASSETS_DIR "neptune.jpg"}, {230.0f, 0.0f, 230.0f}, 4.0f, 5.0f, 0.1f}, // Neptune
+};
+
+std::vector<CorpData> stars{
+    {
+        {ASSETS_DIR "sun.jpg"}, {0.0f, 0.0f, 0.0f}, 20.0f, 0.1f, 0.0f
+    }
 };
 
 int main() {
@@ -47,8 +52,15 @@ int main() {
         data.emplace_back(std::move(planet_ptr), planets[i].position);
     }
 
-    std::unique_ptr<Model> sun_ptr = std::make_unique<Star>(MaterialData{ASSETS_DIR "sun.jpg"}, 20.0f, 1.0f, 0.0f);
-    data.emplace_back(std::move(sun_ptr), glm::vec3{0.0f});
+    for (int i = 0; i < stars.size(); i++) {
+        std::unique_ptr<Model> sun_ptr = std::make_unique<Corp>(
+            stars[i].material,
+            stars[i].radius,
+            stars[i].rotateScaler,
+            stars[i].translateScaler,
+            Constants::FRAG_LIGHT);
+        data.emplace_back(std::move(sun_ptr), stars[i].position);
+    }
 
     window.render(data);
 
--- src/Star.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by lucas.lima on 08/09/2025.
-//
-
-#include "../include/Star.h"
-#include "Constants.h"
-
-Star::Star(MaterialData mat, float radius, float r, float t) :
- Corp(mat, radius, r, t, Constants::FRAG_LIGHT) {}

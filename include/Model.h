ff2de2b856e7ecd329d7f0d57ac526eed4096ecd
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2026-01-15 20:32:25 -0300

    refactor: Remove unused methods from model

--- include/Model.h
+++ include/Model.h
@@ -14,19 +14,13 @@ class Model {
     std::vector<std::shared_ptr<Material>> materials;
     std::shared_ptr<Program> program = nullptr;
 
-    void saveBuffer();
-
-    void genVertexArrayBuffer();
-
-    void bindGeometry() const;
-
-    void bindTexture() const;
-
   public:
     Model(std::shared_ptr<Program> program);
 
     virtual ~Model() = default;
 
+    virtual void draw();
+  
     void loadData(std::shared_ptr<Mesh> mesh, std::shared_ptr<Material> material = nullptr);
 
     void deleteData(size_t index);
@@ -34,7 +28,5 @@ class Model {
     void setProgram(std::shared_ptr<Program> prg);
 
     std::shared_ptr<Program> getModelProgram() const;
-
-    virtual void draw();
 };
 } // namespace oriongl::graphics

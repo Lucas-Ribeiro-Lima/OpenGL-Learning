f86e7707da98faef50d6a9872e45d2ee04fa93c8
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2026-01-16 23:20:18 -0300

    refactor: Add a custom main function to gtest.

---
+++ tests/test_main.cpp
@@ -0,0 +1,22 @@
+#include <WindowSystem.h>
+#include <gtest/gtest.h>
+#include <memory.h>
+
+namespace oriongl::test {
+class Environment : public ::testing::Environment {
+    std::unique_ptr<core::WindowSystem> ctx = nullptr;
+
+    void SetUp() override { ctx = std::make_unique<core::WindowSystem>(); };
+
+    void TearDown() override { ctx->closeWindow(); }
+
+    static Environment *ctx_;
+};
+
+} // namespace oriongl::test
+
+int main(int argc, char **argv) {
+    ::testing::AddGlobalTestEnvironment(new oriongl::test::Environment);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}

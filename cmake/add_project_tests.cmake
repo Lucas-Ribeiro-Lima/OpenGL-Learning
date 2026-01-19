a426d13945c255d61b679d287f6407303a472c3b
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2026-01-19 19:05:51 -0300

    feat: Textures initialization tests.

--- cmake/add_project_tests.cmake
+++ cmake/add_project_tests.cmake
@@ -1,11 +1,14 @@
 enable_testing()
 
-## Search for the tests source code
+# Search for the tests source code
 file(GLOB_RECURSE TESTS_SOURCES "${PROJECT_SOURCE_DIR}/tests/test_*.cpp")
 
-## Configure the test executable
+# Configure the test executable
 set(PROJECT_TESTS_EXEC "${PROJECT_NAME}-tests")
 add_executable(${PROJECT_TESTS_EXEC} ${TESTS_SOURCES})
 target_link_libraries(${PROJECT_TESTS_EXEC} PRIVATE ${PROJECT_CORE_LIB} gtest)
+
+file(COPY "${CMAKE_SOURCE_DIR}/tests/assets/"
+     DESTINATION "${CMAKE_BINARY_DIR}/assets/")
 
 add_test(NAME gtest COMMAND ${PROJECT_TESTS_EXEC})
---
+++ tests/test_texture.cpp
@@ -0,0 +1,34 @@
+#include <Texture.h>
+#include <glad.h>
+#include <gtest/gtest.h>
+
+#define DUMMY_TEXTURE_PATH "assets/black_pixel.png"
+
+namespace oriongl::graphics {
+
+TEST(TextureTest, create_and_initializate) {
+    Texture tex{DUMMY_TEXTURE_PATH};
+
+    EXPECT_NE(tex.getTex(), 0);
+    EXPECT_TRUE(glIsTexture(tex.getTex()));
+};
+
+TEST(TextureTest, create_and_initializate_multiple) {
+    Texture array[] = {{DUMMY_TEXTURE_PATH}, {DUMMY_TEXTURE_PATH}, {DUMMY_TEXTURE_PATH}};
+
+    for (auto &tex : array) {
+        EXPECT_TRUE(glIsTexture(tex.getTex()));
+    }
+
+    for (size_t i = 0; i < 2; i++) {
+        for (size_t j = i + 1; j < 3; j++) {
+            EXPECT_NE(array[i].getTex(), array[j].getTex());
+        }
+    }
+}
+
+TEST(TextureTest, inexistent_texture_file_must_throw) {
+    EXPECT_ANY_THROW(({ Texture tex{"Inexistent_texture_path"}; }));
+}
+
+} // namespace oriongl::graphics

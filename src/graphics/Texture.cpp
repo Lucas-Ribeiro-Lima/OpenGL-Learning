2bf4ebe13cc0c19052934c5b45eb02b66553197a
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2026-01-19 19:05:29 -0300

    refactor: Texture throws exception when a texture fails to load.

--- src/graphics/Texture.cpp
+++ src/graphics/Texture.cpp
@@ -1,42 +1,39 @@
 #include "Texture.h"
 
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #include <glad.h>
 
-
 #define GL_STB_NULL_PLACEHOLDER 0
-//Map for the colors system of images of stb_images
+// Map for the colors system of images of stb_images
 
 namespace oriongl::graphics {
-    constexpr int colorSystem[4] = {GL_STB_NULL_PLACEHOLDER, GL_STB_NULL_PLACEHOLDER, GL_RGB, GL_RGBA};
+constexpr int colorSystem[4] = {GL_STB_NULL_PLACEHOLDER, GL_STB_NULL_PLACEHOLDER, GL_RGB, GL_RGBA};
 
-    Texture::Texture(std::string path) {
-        int width, heigth, nrChannels;
-        stbi_set_flip_vertically_on_load(true);
-        unsigned char *data = stbi_load(path.c_str(), &width, &heigth, &nrChannels, 0);
+Texture::Texture(std::string path) {
+    int width, heigth, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load(path.c_str(), &width, &heigth, &nrChannels, 0);
 
-        glGenTextures(1, &TEX);
-        glBindTexture(GL_TEXTURE_2D, TEX);
+    glGenTextures(1, &TEX);
+    glBindTexture(GL_TEXTURE_2D, TEX);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-        if (data) {
-            glTexImage2D(GL_TEXTURE_2D, 0, colorSystem[nrChannels - 1], width, heigth, 0, colorSystem[nrChannels - 1],
-                         GL_UNSIGNED_BYTE, data);
-            glGenerateMipmap(GL_TEXTURE_2D);
-        } else {
-            glDeleteTextures(1, &TEX);
-        }
-
-        stbi_image_free(data);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, colorSystem[nrChannels - 1], width, heigth, 0, colorSystem[nrChannels - 1],
+                     GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        glDeleteTextures(1, &TEX);
+        throw std::runtime_error{"Failure loading the texture. ABORTING"};
     }
 
-    GLuint Texture::getTex() const {
-        return TEX;
-    }
+    stbi_image_free(data);
 }
+
+GLuint Texture::getTex() const { return TEX; }
+} // namespace oriongl::graphics

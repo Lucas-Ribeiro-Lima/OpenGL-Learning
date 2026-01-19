fb90159501c2af5a52b4bed18e5b208b98a000d2
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2026-01-19 09:01:36 -0300

    refactor: Add new mesh tests

--- tests/test_mesh.cpp
+++ tests/test_mesh.cpp
@@ -11,27 +11,31 @@ indexes_array indexes = {0, 1, 2};
 TEST(MeshTest, create_a_mesh) {
     Mesh mesh{vertexes, indexes};
     EXPECT_NE(mesh.getVAO(), 0);
+    EXPECT_TRUE(glIsVertexArray(mesh.getVAO()));
 }
 
 TEST(MeshTest, create_multiple_meshes) {
-    Mesh mesh1{vertexes, indexes};
-    Mesh mesh2{vertexes, indexes};
-    Mesh mesh3{vertexes, indexes};
+    Mesh mesh_arr[] = {
+        {vertexes, indexes},
+        {vertexes, indexes},
+        {vertexes, indexes},
+    };
 
-    EXPECT_NE(mesh1.getVAO(), 0);
-    EXPECT_NE(mesh2.getVAO(), 0);
-    EXPECT_NE(mesh3.getVAO(), 0);
-}
+    for (size_t it = 0; it < 3; it++) {
+        EXPECT_NE(mesh_arr[it].getVAO(), 0);
+        EXPECT_TRUE(glIsVertexArray(mesh_arr[it].getVAO()));
+    }
+
+    for (size_t i = 0; i < 2; i++) {
+        for (size_t j = i + 1; j < 3; j++) {
+            EXPECT_NE(mesh_arr[i].getVAO(), mesh_arr[j].getVAO());
+        }
+    }
+} // namespace oriongl::graphics
 
 TEST(MeshTest, correct_index_size) {
     Mesh mesh{vertexes, indexes};
     EXPECT_EQ(mesh.getIndexSize(), 3);
-}
-
-TEST(MeshTest, is_a_vertex_array_buffer_) {
-    Mesh mesh{vertexes, indexes};
-
-    EXPECT_TRUE(glIsVertexArray(mesh.getVAO()));
 }
 
 } // namespace oriongl::graphics

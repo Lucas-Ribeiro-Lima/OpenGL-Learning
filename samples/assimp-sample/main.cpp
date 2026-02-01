ce78e45c665d8e5502610e2e5d53094fc83ed86d
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2026-02-01 17:49:41 -0300

    Integrate changes

--- samples/assimp-sample/main.cpp
+++ samples/assimp-sample/main.cpp
@@ -10,15 +10,23 @@ const char frag_shader[] = {
     , '\0'};
 
 const char *backpack_src = "assets/survival_guitar_backpack/scene.gltf";
+const char *dark_knight_src = "assets/dark_knight/scene.gltf";
+const char *sea_keep_src = "assets/sea_keep/scene.gltf";
 
 int main() {
     oriongl::Engine engine;
     oriongl::core::ResourceSystem resource_system;
 
     auto program = resource_system.createShader(vertex_shader, frag_shader);
-    auto backpack = resource_system.createModel("SURVIVAL_BACKPACK", program, backpack_src);
 
-    engine.loadEntityToScene(backpack, std::vector<glm::vec3>{{0.0f, 0.0f, -550.0f}});
+    // auto knight = resource_system.createModel("dark_knight", program, dark_knight_src);
+    // engine.loadEntityToScene(knight, std::vector<glm::vec3>{{0.0f, 100.0f, 150.0f}});
+
+    // auto backpack = resource_system.createModel("backpack", program, backpack_src);
+    // engine.loadEntityToScene(backpack, std::vector<glm::vec3>{{0.0f, 0.0f, -500.0f}});
+
+    auto sea_keep = resource_system.createModel("SEA_KEEP", program, sea_keep_src);
+    engine.loadEntityToScene(sea_keep, std::vector<glm::vec3>{{0.0f, -300.0f, -500.0f}});
 
     engine.run();
     return 0;
